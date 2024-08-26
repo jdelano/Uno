214e4210db117de92c4c7fa6f9ab5016f1e6388b
jdelano <jdelano@cedarville.edu>
2024-08-26 14:15:51 -0400

    Initial Commit

---
+++ Uno/UnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnoApp.swift
+//  Uno
+//
+//  Created by John Delano on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct UnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
